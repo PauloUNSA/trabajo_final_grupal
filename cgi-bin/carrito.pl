40488aee1ddc3e95646ac635ae7dc460e506380b
Gonzalo01703 <glaymem@unsa.edu.pe>
2022-12-24 15:03:47 -0500

    Se creo perl carrito

---
+++ cgi-bin/carrito.pl
@@ -0,0 +1,65 @@
+#!"c:/Strawberry/perl/bin/perl.exe"
+
+   use strict;
+   use warnings;
+   use DBI;
+   use CGI;
+    use utf8;
+print "Content-type: text/html\n\n";
+print <<HTML;
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <link rel="stylesheet" type="text/css" href="../estiloTabla.css">
+    <title>Carrito de Compras</title>
+</head>
+<body>
+    <div class="contenedor-resultado" style="width: 1000px;padding-bottom:35px;">
+    <div class="tabla-resultados">
+HTML
+
+my $q =CGI->new;
+  my $user = 'alumno';
+  my $password = 'pweb1';
+  my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.9";
+  
+  my $dbh = DBI ->connect($dsn,$user,$password) or die ("No se pudo conectar");
+
+
+my $Id=$q->param("id");
+
+    formulario();
+
+
+  my $sth = $dbh->prepare("SELECT Nombre,Precio FROM Productos WHERE Id=?" );
+
+  $sth->execute($Id) or die "error";
+ 
+ print <<HTML;
+    <div class="columna cabecera">
+      <div>Nombre</div>
+      <div>Precio</div>
+    </div>
+HTML
+
+ while(my @row=$sth->fetchrow_array){
+    print"<div class='columna'><div class='izquierdo'> $row[0] </div> \n <div> $row[1] </div></div>";
+  }
+ 
+  $sth ->finish;
+  $dbh->disconnect;                         
+
+
+
+  sub formulario{
+
+
+    print <<HTML;
+        <div>
+            <form action="carrito.pl" method="GET">
+            </form>
+        </div>    
+HTML
+     
+}
