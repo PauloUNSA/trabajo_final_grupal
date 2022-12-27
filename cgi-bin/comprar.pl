eab2464a1fcd7748d6ae1d7cadd836743acf7843
alexis <almamanima@unsa.edu.pe>
2022-12-27 17:08:09 +0000

    agregando tabla de las ventas y poniendo boton del carrito en el menu

--- cgi-bin/comprar.pl
+++ cgi-bin/comprar.pl
@@ -26,22 +26,38 @@ print <<HTML;
           <li><input type="button" onclick="location.href='../cell.html'" value=" Celulares "/></li>
           <li><input type="button" onclick="location.href='../lap.html'" value=" Laptops "/></li>
           <li><input type="button" onclick="location.href='../login.html'" value=" Login "/></li>
-          <img src="./img/logo.png" width="60" height="60" alt="" ></li>
+          <img src="../img/logo.png" width="60" height="60" alt="" ></li>
         </ul>
       </nav>
     </div>
-
+</body>
 HTML
+my $q =CGI->new;
+my $Id=1;
 my $user = 'alumno';
 my $password = 'pweb1';
-my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.9";
+my $dsn = "DBI:MariaDB:database=pweb1;host=127.0.0.1";
 
 my $dbh = DBI ->connect($dsn,$user,$password) or die ("No se pudo conectar");
-my $sth = $dbh->prepare("Select Nombre,Precio from productosVendidos");
+my $sth = $dbh->prepare("select Nombre,Precio from productosVendidos where Id=?");
 #Este Id va a ser el que identrifique al usuario 
+$sth->execute($Id);
+print <<HTML;
+<table id="customers">
+  <tr>
+      <th>Producto</th>
+      <th>Precio</th>
+HTML
 while(my @row=$sth->fetchrow_array){
-    print"<div class='columna'><div class='izquierdo'> $row[0] </div> \n <div> $row[1] </div></div>";
     
+    print <<HTML;
+     <link rel="stylesheet" type="text/css" href="../css/estiloCompra.css">
+  </tr>
+  <tr>
+      <td>$row[0]</td>
+      <td>$row[1]</td>
+  </tr>
+HTML
   }
 
 $sth ->finish;
